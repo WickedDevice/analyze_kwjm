5bd8fa74cb3c0bf92f71591c4c701066824b1ea5
Victor Aprea <victor.aprea@wickeddevice.com>
2016-11-21 15:09:34 -0500

    more tweaking of values

--- analyze_kwjm.js
+++ analyze_kwjm.js
@@ -20,6 +20,9 @@ let input_filename = argv.i || "usb0.csv";
 let stiffness_pole1 = argv.s1 || 0.05;
 let stiffness_pole2 = argv.s2 || stiffness_pole1;
 let epsilon = argv.eps || 0.008;
+let leading_discard_fraction = argv.ld || 0.50;
+let trailing_discard_fraction = argv.td || 0.20;
+
 // check to see if the input file exists and if not exit with an error message and usage
 let input = null;
 try {
@@ -84,15 +87,18 @@ parse(input, {columns: true}, (err, csv) => {
   let level_state = thresholded_temperature_slopes[0];
   let last_edge_idx = 0;
   const minimum_samples_between_edges = 50;
-  let debounced_thresholding = [];
-  thresholded_temperature_slopes.forEach((val, idx) => {
+  // let debounced_thresholding = [];
+  thresholded_temperature_slopes = thresholded_temperature_slopes.map((val, idx) => {
+    let new_val = level_state;
     if(val !== level_state){
       if(idx - last_edge_idx > minimum_samples_between_edges) {
         if (val == 0) {
-          falling_edges.push(idx);
+          falling_edges.push(idx + 1);
+          new_val = 0;
         }
         else {
-          rising_edges.push(idx);
+          rising_edges.push(idx + 1);
+          new_val = 1;
         }
         level_state = val;
         last_edge_idx = idx;
@@ -102,13 +108,62 @@ parse(input, {columns: true}, (err, csv) => {
         console.log("warning: ignoring spurious transition at idx " + idx);
       }
     }
-    debounced_thresholding.push(level_state);
+    return new_val;
+    // debounced_thresholding.push(level_state);
   });
 
   // make sure the index of the first falling edge is after the first detected rising edge
   while(rising_edges[0] >= falling_edges[0] && falling_edges.length > 0){
     falling_edges = falling_edges.slice(1);
   }
+
+  // make those two vectors the same length
+  let min_l = Math.min(rising_edges.length, falling_edges.length);
+  rising_edges = rising_edges.slice(0, min_l);
+  falling_edges = falling_edges.slice(0, min_l);
+
+  // clean the debounced thresholding array up by:
+  // (1) setting value to zero up to the first rising edge
+  // (2) moving each rising edge forward half the distance to it's corresponding falling edge
+  // (3) backing off each falling edge by 5% of the window
+
+  for(let ii = 0; ii < falling_edges.length; ii++){
+    let num_samples = falling_edges[ii] - rising_edges[ii];
+    let new_rising_edge = Math.floor(rising_edges[ii] + (leading_discard_fraction * num_samples));
+    let new_falling_edge = Math.floor(falling_edges[ii] - trailing_discard_fraction * num_samples);
+
+    console.log(`${num_samples} : ${rising_edges[ii]} -> ${falling_edges[ii]}, ${new_rising_edge} -> ${new_falling_edge} `);
+
+    if(new_rising_edge > new_falling_edge){
+      console.log(`failure: major problem, nothing left after removing leading and trailing segments from segment ${rising_edges[ii]} ... ${falling_edges[ii]}`)
+    }
+    else{
+      rising_edges[ii] = new_rising_edge;
+      falling_edges[ii] = new_falling_edge;
+    }
+  }
+
+  // expand the edge events into a vector
+  let debounced_thresholding = [];
+  for(let ii = 0; ii < rising_edges[0] - 1; ii++){
+    debounced_thresholding.push(0);
+  }
+  for(let ii = 0; ii < rising_edges.length - 1; ii++){
+    while(debounced_thresholding.length < falling_edges[ii] - 1){
+      debounced_thresholding.push(1);
+    }
+
+    while(debounced_thresholding.length < rising_edges[ii + 1] - 1){
+      debounced_thresholding.push(0);
+    }
+  }
+  while(debounced_thresholding.length < falling_edges[rising_edges.length - 1] - 1){
+    debounced_thresholding.push(1);
+  }
+  while(debounced_thresholding.length < thresholded_temperature_slopes.length){
+    debounced_thresholding.push(0);
+  }
+
 
   console.log(rising_edges, falling_edges);
 
